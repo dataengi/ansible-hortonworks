3ce02daf458c2efaff5e31aa976654b8fb281ee1
Andriy Lutskiv <alutskiv@scalhive.com>
2018-10-01 18:33:09 +0300

    Terraform support added

---
+++ terraform/live/stage/vars.tf
@@ -0,0 +1,23 @@
+variable "username" {
+  description = "Openstack username"
+}
+
+variable "tenantname" {
+  description = "The name of the Tenant (in terms of OStack tenant is project-name)"
+}
+
+variable "password" {
+  description = "The password for the Tenant."
+}
+
+variable "openstack_auth_url" {
+  description = "The endpoint url to connect to OpenStack."
+}
+
+variable "openstack_keypair" {
+  description = "The keypair to be used."
+}
+
+variable "network_name" {
+  description = "The network to be used."
+}
