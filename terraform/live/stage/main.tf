b3f735c799e86a387908035e5d322d6f5fca9786
Andriy Lutskiv <alutskiv@scalhive.com>
2018-11-02 15:01:28 +0200

    hdp3-3master-nodes-w-druid_atlas_knox_log

--- terraform/live/stage/main.tf
+++ terraform/live/stage/main.tf
@@ -21,8 +21,29 @@ provider "aws" {
 
 module "hdp-master" {
   source                = "../../modules/nodegroups"
-  host_group            = "hdp-master"
-  hostname              = "cluster-os-m"
+  host_group            = "hdp-masternode-01"
+  hostname              = "cluster-os-m1"
+  domainsuffix          = "scalhive.com"
+  nodescount            = 1
+  flavor                = "c4.4xlarge"
+  image                 = "CentOS-7.4"
+  network_name          = "${var.network_name}"
+  admin_username        = "centos"
+  keyname               = "${var.openstack_keypair}"
+  private_key           = "~/.ssh/big-data-sandbox.pem"
+  enable_persist_volume = true
+  aws_zone_id           = "${var.aws_zone_id}"
+  system_volume_size    = 100
+  persist_volume_size   = 30
+  sec_groups            = ["default","local-network"]
+  enable_floating_ip    = false
+}
+
+
+module "hdp-master2" {
+  source                = "../../modules/nodegroups"
+  host_group            = "hdp-masternode-02"
+  hostname              = "cluster-os-m2"
   domainsuffix          = "scalhive.com"
   nodescount            = 1
   flavor                = "c4.4xlarge"
@@ -41,10 +62,10 @@ module "hdp-master" {
 
 module "hdp-slave" {
   source                = "../../modules/nodegroups"
-  host_group            = "hdp-slave"
+  host_group            = "hdp-worker"
   hostname              = "cluster-os-s"
   domainsuffix          = "scalhive.com"
-  nodescount            = 2
+  nodescount            = 3
   flavor                = "c4.2xlarge"
   image                 = "CentOS-7.4"
   network_name          = "${var.network_name}"
@@ -60,17 +81,17 @@ module "hdp-slave" {
 
 module "hdp-edge" {
   source                = "../../modules/nodegroups"
-  host_group            = "hdp-edge"
-  hostname              = "bds-e"
+  host_group            = "hdp-management"
+  hostname              = "cluster-os-e"
   domainsuffix          = "scalhive.com"
-  nodescount            = 0
+  nodescount            = 1
   flavor                = "c4.2xlarge"
   image                 = "CentOS-7.4"
   network_name          = "${var.network_name}"
   admin_username        = "centos"
   keyname               = "${var.openstack_keypair}"
   private_key           = "~/.ssh/big-data-sandbox.pem"
-  enable_persist_volume = false
+  enable_persist_volume = true
   aws_zone_id           = "${var.aws_zone_id}"
   system_volume_size    = 100
   persist_volume_size   = 20
@@ -80,11 +101,10 @@ module "hdp-edge" {
 
 resource "null_resource" "create_inventory" {
 
-
-  depends_on = ["module.hdp-edge", "module.hdp-master", "module.hdp-slave"]
+  depends_on = ["module.hdp-edge", "module.hdp-master", "module.hdp-master2", "module.hdp-slave"]
 
   provisioner "local-exec" {
     when = "create"
-    command = "echo '${module.hdp-edge.nodetype}${module.hdp-edge.static_inventory}${module.hdp-master.nodetype}${module.hdp-master.static_inventory}${module.hdp-slave.nodetype}${module.hdp-slave.static_inventory}' > ../../../inventory/static"
+    command = "echo '${module.hdp-edge.nodetype}${module.hdp-edge.static_inventory}${module.hdp-master.nodetype}${module.hdp-master.static_inventory}${module.hdp-master2.nodetype}${module.hdp-master2.static_inventory}${module.hdp-slave.nodetype}${module.hdp-slave.static_inventory}' > ../../../inventory/static"
     }
 }
